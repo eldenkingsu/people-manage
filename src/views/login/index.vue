1ffcfb3f6562e6aadd950d6d80bf1c37daab4739
yangjunyi <1749911037@qq.com>
2024-05-10 23:07:58 +0800

    3.手机号与密码的校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,12 +6,12 @@
       <el-card shadow="never" class="login-card">
         <!--登录表单-->
         <!-- el-form => el-form-item >el- input-->
-        <el-form>
-          <el-form-item>
-            <el-input placeholder="请输入手机号" />
+        <el-form ref="form" :model="loginForm" :rules="loginRules">
+          <el-form-item prop="mobile">
+            <el-input v-model="loginForm.mobile" placeholder="请输入手机号" />
           </el-form-item>
-          <el-form-item>
-            <el-input placeholder="请输入密码" />
+          <el-form-item prop="password">
+            <el-input v-model="loginForm.password" show-password placeholder="请输入密码" />
           </el-form-item>
           <el-form-item>
             <el-checkbox>用户平台使用协议</el-checkbox>
@@ -26,7 +26,41 @@
 </template>
 <script>
 export default {
-  name: 'Login'
+  name: 'Login',
+  data() {
+    return {
+      loginForm: {
+        mobile: '',
+        password: '',
+        isAgree: false
+      },
+      loginRules: {
+        mobile: [{
+          required: 'true',
+          message: '请输入手机号',
+          trigger: 'blur'
+        }, {
+          pattern: /^1[3-9]\d{9}$/,
+          message: '手机号格式不正确',
+          trigger: 'blur'
+        }
+        ],
+        password: [
+          {
+            required: 'true',
+            message: '请输入密码',
+            trigger: 'blur'
+          }, {
+            min: 6,
+            max: 10,
+            message: '密码长度应为6到16之间',
+            trigger: 'blur'
+          }
+        ],
+        isAgree: []
+      }
+    }
+  }
 }
 </script>
 <style lang="scss">
